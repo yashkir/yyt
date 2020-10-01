822f5b433379a09e92a8bd3fa76e3fdf4e157c41
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-10-01 10:38:29 -0400

    indenting

--- src/yyt.ts
+++ src/yyt.ts
@@ -9,43 +9,43 @@ const VALID_COMMANDS = ['ls', 'add', 'do', 'resetdb', 'dumpdb'];
 backend.init();
 
 yargs
-.usage("Usage: $0 <command> <id/text>")
-.command('ls', 'list all tasks', {}, () => {
-    backend.show((rows) => {
-        rows.forEach((row) => {
-            console.log(`${row.done} ${row.id}: ${row.text}`);
+    .usage("Usage: $0 <command> <id/text>")
+    .command('ls', 'list all tasks', {}, () => {
+        backend.show((rows) => {
+            rows.forEach((row) => {
+                console.log(`${row.done} ${row.id}: ${row.text}`);
+            });
+        })
+    })
+    .command('add <task>', "Add a task to the task list.", {}, (argv) => {
+        console.log(`adding task: ${argv.task}`);
+        backend.add(argv.task as string);
+    })
+    .command('do <task_id>', "mark a task as done", {}, (argv) => {
+        console.log(`Doing task: ${argv.task_id}`);
+        backend.done(argv.task_id as number);
+    })
+    .command('resetdb', "reset the database", {}, () => {
+        const rl = readline.createInterface(process.stdin, process.stdout);
+
+        rl.write("RESETTING THE DATABASE\n");
+        rl.question("Are you sure? (yes/NO):", (answer) => {
+            if(answer == 'yes') {
+                backend.reset();
+            }
+            rl.close();
         });
     })
-})
-.command('add <task>', "Add a task to the task list.", {}, (argv) => {
-    console.log(`adding task: ${argv.task}`);
-    backend.add(argv.task as string);
-})
-.command('do <task_id>', "mark a task as done", {}, (argv) => {
-    console.log(`Doing task: ${argv.task_id}`);
-    backend.done(argv.task_id as number);
-})
-.command('resetdb', "reset the database", {}, () => {
-    const rl = readline.createInterface(process.stdin, process.stdout);
-
-    rl.write("RESETTING THE DATABASE\n");
-    rl.question("Are you sure? (yes/NO):", (answer) => {
-        if(answer == 'yes') {
-            backend.reset();
+    .command('dumpdb', "Output the whole database", {},
+        () => {
+            backend.dump();
+        })
+    .demandCommand(1)
+    .check((argv, options) => {
+        if (VALID_COMMANDS.indexOf(argv._[0]) < 0) {
+            throw new Error(`Invalid command: "${argv._[0]}"`);
+        } else {
+            return true;
         }
-        rl.close();
-    });
-})
-.command('dumpdb', "Output the whole database", {},
-    () => {
-        backend.dump();
     })
-.demandCommand(1)
-.check((argv, options) => {
-    if (VALID_COMMANDS.indexOf(argv._[0]) < 0) {
-        throw new Error(`Invalid command: "${argv._[0]}"`);
-    } else {
-        return true;
-    }
-})
-.argv;
+    .argv;
