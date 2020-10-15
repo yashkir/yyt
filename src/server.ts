a11ab1f6f6a8775693384cd9e12dd8de4c46b6de
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-10-14 20:50:06 -0400

    Red done and tasks rendering.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
 import express = require('express');
 import handlebars = require('handlebars');
-import fs = require('fs')
+import fs = require('fs');
+import backend = require('./backend');
 
-const app = express()
-const port = 8080
+const DBPATH = '/home/yashkir/tmp/test.db'; //TODO move this out
+const app = express();
+const port = 8080;
 
 let t1 = handlebars.compile(fs.readFileSync('views/index.html').toString());
+let t2 = handlebars.compile(fs.readFileSync('views/tasks.html').toString());
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.send(t1( { name: 'test'} ));
+});
+
+app.get('/tasks', (req, res) => {
+    backend.init(DBPATH);
+    backend.list((tasks) => {
+        res.send(t2( { tasks: tasks } ));
+    });
 });
 
 app.listen(port, () => {
