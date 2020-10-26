bda774c7cd057e42e33342877879e21ffe94560c
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-10-26 11:19:16 -0400

    Update test_curl.sh

--- test_curl.sh
+++ test_curl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo -e "\n-- TEST: Logging in..."
 curl http://localhost:8080/login -c tmp/cookie.txt -b tmp/cookie.txt -H 'Content-Type: application/json' -d '{"username": "test", "password": "password"}' -L
-curl http://localhost:8080/authtest -c tmp/cookie.txt -b tmp/cookie.txt -v -L
+echo -e "\n-- TEST: Trying authtest/ ..."
+curl http://localhost:8080/authtest -c tmp/cookie.txt -b tmp/cookie.txt -L
+echo -e "\n-- TESTS DONE"
 
